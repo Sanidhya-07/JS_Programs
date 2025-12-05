d46e0d4e2ab397a5c6f01c42ed0af55e194f81b2
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-12-05 11:29:47 +0530

    Square root of number

---
+++ 2_squareroot_of_number.js
@@ -0,0 +1,10 @@
+function squareRoot(number) {
+  const ans = Math.sqrt(number);
+  return ans;
+}
+
+result = squareRoot(100);
+console.log(result);
+
+result = squareRoot(50);
+console.log(result);
