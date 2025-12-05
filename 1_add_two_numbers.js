28f49e04cffc01bd74f595897ec0c810db89eb6a
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-12-05 11:06:55 +0530

    Add two numbers

---
+++ 1_add_two_numbers.js
@@ -0,0 +1,15 @@
+// Add two numbers...
+
+function addition(num1, num2) {
+  add = num1 + num2;
+  return add;
+}
+
+result = addition(20, 15);
+console.log(result);
+
+result = addition(50, 8);
+console.log(result);
+
+result = addition(28, 69);
+console.log(result);
