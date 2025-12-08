1ab5074fb7904bbfbec407d355a30bd49bfeeac9
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-12-08 16:07:03 +0530

    Odd Even number

---
+++ 6_odd_even.js
@@ -0,0 +1,10 @@
+// number is Odd or Even
+
+cheakNumber = (number) => {
+  if (number % 2 === 0) {
+    return `${number} is EVEN.`;
+  } else {
+    return `${number} is ODD.`;
+  }
+};
+console.log(cheakNumber(0));
